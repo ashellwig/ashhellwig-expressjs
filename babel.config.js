c4aa287df40fe8d19a660586a36227da59d1f934
Ash Hellwig <ahellwig.dev@gmail.com>
2020-05-23 03:25:16 -0600

    add express-graphql, babel, eslint, express, gql

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = function (api) {
+  // Presets
+  const presets = [
+    '@babel/preset-env'
+  ]
+
+  // Plugins
+  const plugins = []
+
+  return {
+    presets,
+    plugins
+  }
+}
