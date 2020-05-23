e96013565e0486974fbbc9bba2d0f5996a0451b9
Ash Hellwig <ahellwig.dev@gmail.com>
2020-05-23 03:58:39 -0600

    remove yargs

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
 module.exports = function (api) {
   // Presets
   const presets = [
-    '@babel/preset-env'
+    '@babel/preset-env',
+    '@babel/preset-flow'
   ]
 
   // Plugins
--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
+// --- Import Libraries ---
+// Polyfill
 import 'core-js/stable'
 import 'regenerator-runtime/runtime.js'
+
+// Command line argument parser
+import * as argv from 'yargs'
+
+argv.count('verbose').alias('v', 'verbose')
+
+const VERBOSE_LEVEL = argv.verbose
+
+function WARN () { VERBOSE_LEVEL >= 0 && console.log.apply(console, arguments) }
+function INFO () { VERBOSE_LEVEL >= 1 && console.log.apply(console, arguments) }
+function DEBUG () { VERBOSE_LEVEL >= 2 && console.log.apply(console, arguments) }
