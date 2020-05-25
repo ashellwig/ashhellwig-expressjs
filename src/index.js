17b5f3c357310b550861b1058095408d693f7037
Ash Hellwig <ahellwig.dev@gmail.com>
2020-05-25 03:06:03 -0600

    add some server logic

--- src/index.js
+++ src/index.js
@@ -2,3 +2,28 @@
 // Polyfill
 import 'core-js/stable'
 import 'regenerator-runtime/runtime.js'
+
+import express from 'express'
+import expressGraphQL from 'express-graphql'
+import {
+    buildSchema
+} from 'graphql'
+
+const schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`)
+
+const root = {
+    message: () => 'Hello, world!'
+}
+
+const app = express()
+app.use('/graphql', expressGraphQL({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}))
+
+app.listen(4000, () => console.log('GQL Server now running on :4000/graphql'))
